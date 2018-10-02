0d386c559f43dc1b8e7c2f4e1d670d9ff8747f94
DORA_XIE <joy8963259@gmail.com>
2018-10-02 17:39:15 +0800

    first commit

---
+++ bottender.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  line: {
+    accessToken: 'qpsTBoLY7crQPCnylF8MtREXE7u6QmVYlPpzNOH/I3mWtUDdBrkCqfhysiFHSdHlC5oCl8I9fosAwgryufCQyNvDue4bcHf4/d2oGaqb1xvkOEU/LZGN+XssZeFdPx2TIXjWxkgdBESFPyTfZ3rZzAdB04t89/1O/w1cDnyilFU=',
+    channelSecret: '9454c58802c9b8745f60efcae380ba2c'
+  }
+};
