0f5b84fe852192623d93c8188ca9bde8ee4e693f
DORA_XIE <joy8963259@gmail.com>
2018-10-03 14:16:15 +0800

    add music action

---
+++ actions/music.js
@@ -0,0 +1,11 @@
+module.exports = async (context, match) => {
+    const musics = ["https://www.youtube.com/watch?v=3Y0Ut5ozaKs","https://www.youtube.com/watch?v=Dnj5Tcpev0Q","https://www.youtube.com/watch?v=m60hjpNtq5k"]
+    const intro = ["「越漂亮的女人越會騙人，電視攏有在講。」回過頭，我才慢慢瞭解。那些年少時的看破，和自以為擁有的愛情，都只是在浪流連。","像是每個男人都有過的曾經 每個世代都有的情感經歷 最偉大的愛情不是永遠 而是曾經在平凡生活裡有你有我的那些深刻","放你自由去啦是我不夠瞭解放你自由去啦是我不夠瞭解你想你點一支煙"]
+
+    var random_index = Math.floor(Math.random()*musics.length)
+    var random_music = musics[random_index];
+    var random_intro = intro[random_index]
+    const { userId, displayName } = context.session.user;
+    console.log(userId + displayName);
+    await context.replyText(`${random_intro}：\n${random_music}`);
+  };
