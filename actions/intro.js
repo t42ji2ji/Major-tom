f565f1add603b7f924a458cac1f1f7b2a96d3ee6
DORA_XIE <joy8963259@gmail.com>
2018-11-02 18:37:37 +0800

    up

--- actions/intro.js
+++ actions/intro.js
@@ -3,7 +3,7 @@ module.exports = async (context, match) => {
         type: 'buttons',
         thumbnailImageUrl: 'https://instagram.ftpe7-4.fna.fbcdn.net/vp/948beb7aee8710b3aadf904a2cd13533/5C547A48/t51.2885-15/e35/33630528_178973942809157_3426440959318032384_n.jpg',
         title: 'Doraralab',
-        text: '喜愛程式與設計，提供世界好用又好看的服務。',
+        text: '實驗室的室長本人，用盡全力改變世界',
         actions: [
           {
             type: 'uri',
