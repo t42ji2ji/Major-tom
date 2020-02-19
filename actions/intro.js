09dfd2af093427e8dbe8c454e3b8bbeaa85484b2
DORA_XIE <joy8963259@gmail.com>
2020-02-19 18:57:10 +0800

    delete fb

--- actions/intro.js
+++ actions/intro.js
@@ -1,25 +1,19 @@
 module.exports = async (context, match) => {
-    await context.replyTemplate('this is a template', {
-        type: 'buttons',
-        thumbnailImageUrl: 'https://instagram.ftpe7-4.fna.fbcdn.net/vp/948beb7aee8710b3aadf904a2cd13533/5C547A48/t51.2885-15/e35/33630528_178973942809157_3426440959318032384_n.jpg',
-        title: 'Doraralab',
-        text: '實驗室的室長本人，用盡全力改變世界',
-        actions: [
-          {
-            type: 'uri',
-            label: 'Medium文章',
-            uri: 'https://medium.com/%E5%93%86%E5%95%A6%E5%AF%A6%E9%A9%97%E5%AE%A4',
-          },
-          {
-            type: 'uri',
-            label: 'FB粉絲專頁',
-            uri: 'https://www.facebook.com/doraralab/',
-          },
-          {
-            type: 'uri',
-            label: '追蹤IG',
-            uri: 'https://www.instagram.com/doraralab/',
-          },
-        ],
-      });
-  };
+  await context.replyTemplate('this is a template', {
+    type: 'buttons',
+    thumbnailImageUrl: 'https://instagram.ftpe7-4.fna.fbcdn.net/vp/948beb7aee8710b3aadf904a2cd13533/5C547A48/t51.2885-15/e35/33630528_178973942809157_3426440959318032384_n.jpg',
+    title: 'Doraralab',
+    text: '實驗室的室長本人，用盡全力改變世界',
+    actions: [{
+        type: 'uri',
+        label: 'Medium文章',
+        uri: 'https://medium.com/%E5%93%86%E5%95%A6%E5%AF%A6%E9%A9%97%E5%AE%A4',
+      },
+      {
+        type: 'uri',
+        label: '追蹤IG',
+        uri: 'https://www.instagram.com/doraralab/',
+      },
+    ],
+  });
+};
--- index.js
+++ index.js
@@ -1,30 +1,35 @@
-const { LineBot, ConsoleBot, MongoSessionStore } = require('bottender');
-const { createServer } = require('bottender/express');
+const {
+  LineBot,
+  ConsoleBot,
+  MongoSessionStore
+} = require('bottender');
+const {
+  createServer
+} = require('bottender/express');
 
 const config = require('./bottender.config.js').line;
 const handler = require('./handler');
 
-const useConsole = false;
-const bot = useConsole
-  ? new ConsoleBot({ 
+
+const useConsole = true;
+const bot = useConsole ?
+  new ConsoleBot({
     fallbackMethods: true,
     sessionStore: new MongoSessionStore(
-      'mongodb://heroku_34smc82j:836jqr8ffo6erv8ngmc6h32afn@ds249233.mlab.com:49233/heroku_34smc82j',
-      {
+      'mongodb://heroku_34smc82j:836jqr8ffo6erv8ngmc6h32afn@ds249233.mlab.com:49233/heroku_34smc82j', {
         collectionName: 'session',
       }
     )
-  })
-  : new LineBot({
-      accessToken: config.accessToken,
-      channelSecret: config.channelSecret,
-      sessionStore: new MongoSessionStore(
-        'mongodb://heroku_34smc82j:836jqr8ffo6erv8ngmc6h32afn@ds249233.mlab.com:49233/heroku_34smc82j',
-        {
-          collectionName: 'session',
-        }, 10
-      )
-    });
+  }) :
+  new LineBot({
+    accessToken: config.accessToken,
+    channelSecret: config.channelSecret,
+    sessionStore: new MongoSessionStore(
+      'mongodb://heroku_34smc82j:836jqr8ffo6erv8ngmc6h32afn@ds249233.mlab.com:49233/heroku_34smc82j', {
+        collectionName: 'session',
+      }, 10
+    )
+  });
 
 bot.setInitialState({
   in_mode: false,
